2d3df35cc41a14a98d38a8eccb00e91943bfcf55
wood.wu <kilokingw@gmail.com>
2023-01-05 23:06:08 +0800

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex gap-4 h-10 text-zinc-400 text-xs justify-center items-center hidden'>
+    <div className='flex gap-4 h-10 text-zinc-400 text-xs justify-center items-center '>
 
       <div>
-        版權所有© 2023 夢想動畫 Moonshine, 國家高速網路與計算中心
+        {/* 版權所有© 2023 夢想動畫 Moonshine, 國家高速網路與計算中心 */}
+        info@moonshine.tw
       </div>
 
     </div>
