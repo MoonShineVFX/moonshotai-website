5119ea990eac9538842b640989bc6ac5ea568f9c
wood.wu <kilokingw@gmail.com>
2023-03-27 16:43:38 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    height: theme => ({
-      auto: 'auto',
-      ...theme('spacing'),
-      full: '100%',
-      screen: 'calc(var(--vh) * 100)',
-    }),
-    minHeight: theme => ({
-      '0': '0',
-      ...theme('spacing'),
-      full: '100%',
-      screen: 'calc(var(--vh) * 100)',
-    }),
+
     extend: {},
   },
   plugins: [],
