4a8091be778769fd454d5e3d5d2a27d465aa971b
wood.wu <kilokingw@gmail.com>
2023-04-01 00:38:07 +0800

    Update index.js

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -11,12 +11,13 @@ import { AnimatePresence,motion } from "framer-motion";
 function Modal(props) {
   return (
     <motion.div 
-      className="modal fixed top-0 left-0 w-full h-screen bg-black/50 z-50" style={{ display: props.show ? "block" : "none" }}
+      className="modal fixed top-0 left-0 w-full h-screen  z-50" style={{ display: props.show ? "block" : "none" }}
 
     >
+      <div className='fixed top-0 left-0 w-full h-screen bg-black/50' onClick={props.onClose}></div>
       {props.show&&
           <motion.div 
-            className="modal-content bg-white w-10/12 md:w-8/12 relative mt-10 py-12 px-10 mx-auto  h-[90vh] overflow-y-auto  overflow-x-hidden"
+            className="modal-content bg-white w-10/12 md:w-8/12 relative mt-10 py-12 px-10 mx-auto  max-h-[90vh] overflow-y-auto  overflow-x-hidden"
             initial={{ opacity: 0,y:0 }}
             animate={{ opacity: 1,y:0 }}
             exit={{ opacity: 0,y:0 }}
@@ -38,22 +39,7 @@ function Modal(props) {
     
             最後，希望使用者能夠理解本服務的宗旨和意圖，並且遵守相關的使用規定。如果您有任何建議，歡迎與我們分享。
             </div>
-            <div className='text-2xl font-bold'>模組介紹</div>
-            <div className='mt-2 mb-8'>
-             <p> 寫實使用了「Realistic Vision V1.3 」這個 Modle，優點在於它可以生成照片類型的圖片，其生成的圖像具有真實的紋理及材質和逼真的色彩，模型涵蓋元素廣，不論是室內外場景、人物肖像、動物都能夠生成。</p>
-    
-              <span>---</span> 
-    
-              <p>插畫使用了「concept_mix」這個 Model，是 Moonshot 團隊為了讓”插畫”能夠有更多樣的元素產出而組合成的 Model ，更容易創造出較戲劇化的構圖、幻想中的物件或場景、3D建模感或水彩、油畫、水墨等藝術類型的風格。</p> 
-    
-              <span>---</span> 
-    
-              <p>漫畫使用了「Anything-V4.5」這個 Model，擅長生成2D風格的圖像，各式動漫風格或動畫感較強的畫面，元素多樣，表現相對穩定且入門難度較低。</p> 
-    
-              <span>---</span> 
-    
-               <p>由於寫實生成的人物較自然，特別新增非常熱門的「chilloutmix」作為寫實人像這個子分項的 Model，擅長繪製人物，細節豐富且質感較佳，整體而言生成的圖象銳利感較強，光線較豐富。</p> 
-            </div>
+
           </motion.div>
       }
 
