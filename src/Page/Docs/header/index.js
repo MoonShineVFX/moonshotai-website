4d5af82e6658111afcbdbd1a7375579479a7c410
wood.wu <kilokingw@gmail.com>
2023-04-19 14:53:43 +0800

    Updated Docs page

--- src/Page/Docs/header/index.js
+++ src/Page/Docs/header/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function index() {
   return (
-    <div className='text-white border-b border-white/50 p-6 w-full'>
+    <div className='text-white border-b border-white/30 p-6 w-full fixed bg-zinc-900 z-50'>
       <div className='w-4/5 mx-auto flex items-center gap-10'>
           <div className='text-3xl font-black w-32'>
             <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="" className='w-full'/>
           </div>
-          <div>documentation</div>
+          <div>Documentation</div>
       </div>
     </div>
   )
--- src/Page/Docs/index.js
+++ src/Page/Docs/index.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { AnimatePresence,motion } from "framer-motion";
 import Header from './header'
-function index() {
+import { animateScroll as scroll, scroller } from 'react-scroll';
+function Index() {
+  const [currentKey, setCurrentKey] = useState(0);
+  const scrollTo = (target) => {
+    scroller.scrollTo(target, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    });
+  };
+  const menuItem = [
+    {title:"關於moonshot",section:"section1"},
+    {title:"指令介紹",section:"section2"},
+    {title:"模型介紹",section:"section3"},
+    {title:"社群規章",section:"section4"},
+    {title:"使用聲明",section:"section5"},
+  ]
   return (
-    <div>
+    <div className=''>
       <Header />
-      <div className='flex justify-start items-start text-white w-10/12 mx-auto'>
-        <div className='  min-h-screen w-1/4 p-10'>
+      <div className='flex justify-start items-start text-white w-8/12 mx-auto pt-20'>
+        <div className='  min-h-screen w-1/4 p-10 fixed'>
           <div className=' font-bold '>Documentation</div>
-          <ul className=' border-l border-white/50 pl-5 mt-4 tracking-widest leading-loose'>
-            <li>
-              關於我們
-            </li>
-            <li>
-              使用聲明
-            </li>
-            <li>..</li>
+          <ul className=' border-l border-white/50 pl-5 mt-4 tracking-wide leading-loose text-normal text-white/50'>
+            {
+              menuItem.map((item,index)=>{
+                return(
+                  <li 
+                    className={' cursor-pointer hover:text-white' + (currentKey === index ? ' text-white' : ' text-white/50')} 
+                    onClick={() => {
+                      scrollTo(item.section)
+                      setCurrentKey(index)
+                    }}
+                  >{item.title}</li>
+                )
+              })
+            }
           </ul>
         </div>
-        <motion.div className=" modal relative w-full min-h-screen w-10/12 md:w-3/4 relative  px-10 mx-auto text-white ">
+        <motion.div className=" modal relative ml-[20%] min-h-screen w-10/12 md:w-3/4  px-10 mx-auto text-white flex-auto overflow-y-auto ">
           <motion.div 
             className=" pt-0 "
             initial={{ opacity: 0,y:0 }}
@@ -30,21 +52,53 @@ function index() {
               delay: 0.5,
             }}
           >  
-            <div className='text-3xl font-black w-1/4 my-16'>
-              <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="" className='w-full'/>
+            <div id="section1" className='min-h-screen pt-28'>
+              <div className='text-3xl font-black w-1/4 mb-16'>
+                <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="" className='w-full'/>
+              </div>
+              <div className='text-2xl font-bold  mb-4'>關於我們</div>
+              <div className='mt-2 mb-8 leading-9 text-white/70'>Moonshot 是一個能夠在 Line 上輕鬆使用的 AI 繪圖工具，可輕易地通過指令切換多個 model 來達到風格轉換，使用「Stable Diffusion」作為運行的基礎，盡可能的在便利與多元使用上取得平衡。
+              希望透過此服務讓更多人能夠認識並了解AI繪圖，所以同時也在 Line 上成立討論社群，鼓勵新手勇於提問老手熱心解惑的互動形式，營造良好學習成長環境。</div>
             </div>
-            <div className='text-2xl font-bold'>關於我們</div>
-            <div className='mt-2 mb-8 leading-9 text-white/70'>Moonshot 是一個能夠在 Line 上輕鬆使用的 AI 繪圖工具，可輕易地通過指令切換多個 model 來達到風格轉換，使用「Stable Diffusion」作為運行的基礎，盡可能的在便利與多元使用上取得平衡。
-            希望透過此服務讓更多人能夠認識並了解AI繪圖，所以同時也在 Line 上成立討論社群，鼓勵新手勇於提問老手熱心解惑的互動形式，營造良好學習成長環境。</div>
-            <div className='text-2xl font-bold'>使用聲明💡 </div>
-            <div className='mt-2 mb-8 leading-9 text-white/70'>
+            <div id="section2" className='min-h-screen  pt-28'>
+              <div className='text-2xl font-bold  mb-4'>指令介紹 </div>
+              <div className='mt-2 mb-8 leading-9 text-white/70'>
+                
+                <p>本服務僅供娛樂與學習使用，意在推廣AI繪圖技術。使用者的行為與本服務無關。</p> 
+                <p>使用者需遵守網路社群使用規定，不得違反社會善良風俗、不使用AI傷害他人、煽動暴力或進行任何惡意行為。</p>
+                <p>我們會在可負擔範圍內盡力維持服務的穩定性，但不做任何保證，如果您在使用的過程中遇到問題，可以透過網頁上的聯絡資訊聯繫我們。</p>
+                <p>最後，希望使用者能夠理解本服務的宗旨和意圖，並且遵守相關的使用規定。如果您有任何建議，歡迎與我們分享。</p>
               
-              <p>本服務僅供娛樂與學習使用，意在推廣AI繪圖技術。使用者的行為與本服務無關。</p> 
-              <p>使用者需遵守網路社群使用規定，不得違反社會善良風俗、不使用AI傷害他人、煽動暴力或進行任何惡意行為。</p>
-              <p>我們會在可負擔範圍內盡力維持服務的穩定性，但不做任何保證，如果您在使用的過程中遇到問題，可以透過網頁上的聯絡資訊聯繫我們。</p>
-              <p>最後，希望使用者能夠理解本服務的宗旨和意圖，並且遵守相關的使用規定。如果您有任何建議，歡迎與我們分享。</p>
-            
+              </div>
             </div>
+            <div id="section3" className='min-h-screen  pt-28'>
+              <h1 className="text-2xl font-bold mb-4">模型介紹</h1>
+              <div className='mt-2 mb-8 leading-9 text-white/70'>
+
+              </div>
+
+            </div>
+
+            <div id="section4" className='min-h-screen  pt-28'>
+              <h1 className="text-2xl font-bold mb-4">社群規章</h1>
+              <div className='mt-2 mb-8 leading-9 text-white/70'>
+
+              </div>
+            </div>
+
+            <div id="section5" className='min-h-screen  pt-28'>
+              <div className='text-2xl font-bold  mb-4'>使用聲明💡 </div>
+              <div className='mt-2 mb-8 leading-9 text-white/70'>
+                
+                <p>本服務僅供娛樂與學習使用，意在推廣AI繪圖技術。使用者的行為與本服務無關。</p> 
+                <p>使用者需遵守網路社群使用規定，不得違反社會善良風俗、不使用AI傷害他人、煽動暴力或進行任何惡意行為。</p>
+                <p>我們會在可負擔範圍內盡力維持服務的穩定性，但不做任何保證，如果您在使用的過程中遇到問題，可以透過網頁上的聯絡資訊聯繫我們。</p>
+                <p>最後，希望使用者能夠理解本服務的宗旨和意圖，並且遵守相關的使用規定。如果您有任何建議，歡迎與我們分享。</p>
+              
+              </div>
+            </div>
+
+
 
           </motion.div>
         </motion.div>
@@ -56,4 +110,4 @@ function index() {
   )
 }
 
-export default index
+export default Index
