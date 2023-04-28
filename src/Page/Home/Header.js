77a15021ca8754e937790bd286b48a676ed0e7e1
wood.wu <kilokingw@gmail.com>
2023-04-28 21:38:19 +0800

    Update Header.js

--- src/Page/Home/Header.js
+++ src/Page/Home/Header.js
@@ -45,7 +45,11 @@ function Header() {
   return (
     <div className='flex flex-col md:flex-row'>
 
-      <div className='w-full md:w-1/2 md:h-screen text-white relative flex items-center justify-center overflow-hidden drop-shadow-2xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#49531F]   to-black-600 ' ref={vantaRef}>
+      <div className='w-full md:w-1/2 h-[70vh] md:h-screen text-white relative flex items-center justify-center overflow-hidden drop-shadow-2xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#49531F]   to-black-600 ' ref={vantaRef}>
+        <div className='text-white font-semibold  text-lg absolute top-5 left-5 z-50 flex gap-5'>
+          <Link to="/gallery" class="rounded-full bg-gradient-to-r from-gray-800 to-gray-500 py-2 px-5 text-base " disabled> Gallery </Link>
+          <Link to="/app" class="rounded-full bg-gradient-to-r from-gray-800 to-gray-500 py-2 px-5 text-base" disabled> Sign In  </Link>
+        </div>
         <motion.div
           initial={{ opacity: 0,y:'15' }}
           animate={{ opacity: 1,y:0 }}
@@ -105,11 +109,8 @@ function Header() {
         
 
       </div>
-      <div className='w-full md:w-1/2 h-[300px] md:h-screen overflow-hidden  relative'>
-        <div className='text-white font-semibold  text-lg absolute top-5 right-5 z-50 flex gap-5'>
-          <Link to="/gallery" class="rounded-full bg-gradient-to-r from-rose-800 to-rose-500 py-2 px-10 " disabled> Gallery </Link>
-          <Link to="/app" class="rounded-full bg-gradient-to-r from-blue-800 to-blue-500 py-2 px-10 " disabled> Sign In  </Link>
-        </div>
+      <div className='w-full md:w-1/2 h-[30vh] md:h-screen overflow-hidden  relative'>
+
         <div className=' w-[800px] md:w-[1500px] brightness-75' id="headerBg">
           <HeaderImagesBg  data={imgData}/>
         </div>
