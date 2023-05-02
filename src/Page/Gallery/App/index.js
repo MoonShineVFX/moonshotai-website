0124bd2a8349566a73326779a0a376bd7080c119
wood.wu <kilokingw@gmail.com>
2023-05-02 17:01:26 +0800

    Update index.js

--- src/Page/Gallery/App/index.js
+++ src/Page/Gallery/App/index.js
@@ -2,17 +2,41 @@ import React, { useState, useEffect }  from 'react'
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import {motion,AnimatePresence} from 'framer-motion'
 import { FiHeart,FiDownload } from "react-icons/fi";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 import Header from '../header'
 import liff from '@line/liff';
+const dropDownManuItem = [
+  {title:"Renders", display:true},
+  {title:"Collection", display:false},
+  {title:"Loved", display:false},
+  {title:"Following",display:false},
+]
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [images, setImages] = useState([]);
   const [imagesResults, setImagesResults] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
   const [currentProfile, setCurrentProfile] = useState(null);
+  const [currentDropDownItem, setCurrentDropDownItem] = useState(dropDownManuItem[0])
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false)
   const imageVariants = {
     hidden: { opacity: 0, },
     visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+  };
+  const dropdownVariants = {
+    open: {
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
   };
   const liffID = process.env.REACT_APP_LIFF_LOGIN_ID
   const fetchUserImages = (profile)=>{
@@ -62,11 +86,15 @@ function App() {
       console.log(error);
     });
   }
+  const toggleDropdown = () => {
+    setIsDropDownOpen(!isDropDownOpen);
+  };
+
   useEffect(() => {
-    // initializeLineLogin()
+    initializeLineLogin()
   }, []);
   return (
-    <div>
+    <div className=" bg-gradient-to-b from-[#49531F]">
       <Header isLoggedIn={isLoggedIn}/>
       <div className='lg:w-10/12 mx-auto lg:my-10'>
 
@@ -84,13 +112,50 @@ function App() {
           }
 
         </div>
-        <div className='grid grid-cols-2 md:grid-cols-4  items-center gap-3 my-10 md:my-5 flex-wrap'>
+        <div className='grid-cols-2 md:grid-cols-4  items-center gap-3 my-10 md:my-5 flex-wrap hidden lg:grid'>
           <div className='bg-zinc-700 hover:bg-zinc-500 text-white rounded-full py-2 px-4 cursor-pointer md:w-auto'>Renders</div>
           <div className='bg-zinc-700 hover:bg-zinc-500 text-white rounded-full py-2 px-4 cursor-not-allowed md:w-auto opacity-30' >Collections</div>
           <div className='bg-zinc-700 hover:bg-zinc-500 text-white rounded-full py-2 px-4 cursor-not-allowed md:w-auto opacity-30'>Loved</div>
           <div className='bg-zinc-700 hover:bg-zinc-500 text-white rounded-full py-2 px-4 cursor-not-allowed md:w-auto opacity-30'>Following</div>
         </div>
-        <div className='my-5'>
+        <div className=' relative p-4 block lg:hidden'>
+          <div 
+            className='text-white rounded-full bg-[#444] px-3 py-1 w-1/2 flex justify-between items-center'
+            onClick={toggleDropdown}
+          >{currentDropDownItem.title} <MdKeyboardArrowDown /></div>
+            <motion.div
+              className={`fixed w-full h-screen top-0 left-0 bg-black/60 z-20 ${isDropDownOpen ? ' ' : ' hidden'}` }
+              variants={dropdownVariants}
+              initial="closed"
+              animate={isDropDownOpen ? 'open' : 'closed'}
+              onClick={toggleDropdown}
+            ></motion.div>
+            <motion.div 
+              className={`text-white  absolute rounded-lg bg-[#444] p-2 mt-2 w-1/3  border-white/20 z-30` }
+              variants={dropdownVariants}
+              initial="closed"
+              animate={isDropDownOpen ? 'open' : 'closed'}
+            
+            >
+              {dropDownManuItem.map((item,index)=>{
+                return(
+                  <div 
+                    key={item.title} 
+                    className='hover:bg-[#555] p-2 text-sm rounded-lg'
+                    onClick={()=>{
+                      setCurrentDropDownItem(item)
+                      toggleDropdown()
+                    }}
+                  >{item.title}</div>
+                )
+              })}
+            </motion.div>
+          
+
+
+        </div>
+
+        <div className='my-10'>
           <div className='text-white text-xl text-center md:text-left md:text-3xl '>Renders {images && images.count} images</div>
           {!imagesResults ?
           <div className='text-white'>Loading</div> 
