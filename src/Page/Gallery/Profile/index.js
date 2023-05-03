124a949e89cc6f1c56ac3d01782084ff49d4a255
wood.wu <kilokingw@gmail.com>
2023-05-03 17:48:52 +0800

    Update index.js

--- src/Page/Gallery/Profile/index.js
+++ src/Page/Gallery/Profile/index.js
@@ -45,14 +45,14 @@ function Index() {
   const dropdownVariants = {
     open: {
       opacity: 1,
-      height:'auto',
+      display:'block',
       transition: {
         duration: 0.2,
       },
     },
     closed: {
       opacity: 0,
-      height:0,
+      display:'none',
       transition: {
         duration: 0.2,
       },
