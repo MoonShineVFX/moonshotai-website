4ec9690cca3e3ef6781c536a189853048d06719b
wood.wu <kilokingw@gmail.com>
2023-06-19 20:49:56 +0800

    Update index.js

--- src/Page/Gallery/header/index.js
+++ src/Page/Gallery/header/index.js
@@ -42,10 +42,10 @@ function Index({currentUser,isLoggedIn}) {
     <div className='  top-0 text-white lg:border-b border-[#3c4756] p-5 w-full  bg-white/10 z-50 flex flex-row flex-wrap 
    justify-between '>
       <div className=' items-center  text-white mr-6 gap-2 pt-1 flex lg:flex'>
-          <div className='font-black w-24 lg:w-32'>
+          <a href='/' className='font-black w-24 lg:w-32'>
             <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="" className='w-full'/>
-          </div>
-          <div className='lg:text-xl'>Gallery</div>
+          </a>
+          <div className='lg:text-xl'></div>
       </div>
       <div className="block lg:hidden ml-auto">
           <button
