e75c78fcc7c44e95c01c783a01f4e2097d3f5ef2
wood.wu <kilokingw@gmail.com>
2023-06-20 18:18:33 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,37 @@ exports.host = functions.https.onRequest((request, response) => {
   const META_PLACEHOLDER = /<meta name="__REPLACE_START__"\/>.*<meta name="__REPLACE_END__"\/>/;
   let indexHTML = fs.readFileSync('./source/index.html').toString();
   const path = request.path ? request.path.split('/') : request.path;
-  const customOpenGraph = `
-      <title>Moonshot 位置在${path[2]}</title>
-      <meta
-        name="description"
-        content="位置在${path[2]}的描述~> Ai art Linebot Group"
-      />
-      <meta property="og:title" content="Moonshot 位置在${path[2]}" />
-      <meta property="og:description" content="針對 ${path[2]} 處理的描述 Ai art Linebot Group" />
-      <meta property="og:image" content="logo.png" />
-    `;
-  
 
+  if(path[1] === 'post'){
 
-  indexHTML = indexHTML.replace(META_PLACEHOLDER, customOpenGraph);
-  response.status(200).send(indexHTML);
+    fetchGalleriesDetail(path[2])
+      .then(gData=>{
+        const {title, description,urls} = gData
+        const customOpenGraph = `
+          <title>Moonshot ${title}</title>
+          <meta
+            name="description"
+            content="${description} "
+          />
+          <meta property="og:title" content=">Moonshot ${title}" />
+          <meta property="og:description" content="${description} " />
+          <meta property="og:image" content=${urls.thumb} />
+        `;
+        indexHTML = indexHTML.replace(META_PLACEHOLDER, customOpenGraph);
+        response.status(200).send(indexHTML);
+
+      })
+      return
+  }
+
 });
 
+const fetchGalleriesDetail = async (id) => {
+  const requestOptions = {
+    method: 'GET',
+    headers:{'Content-Type': 'application/json'}
+  };
+  const response = await fetch(apiUrl+'galleries/'+id ,requestOptions)
+  const data = await response.json()
+  return data
+}
