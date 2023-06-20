d5e894e6557617d3627299fc7c75bbdcbd5d9f66
wood.wu <kilokingw@gmail.com>
2023-06-20 18:00:39 +0800

    wip index.js

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,8 @@ exports.host = functions.https.onRequest((request, response) => {
   const META_PLACEHOLDER = /<meta name="__REPLACE_START__"\/>.*<meta name="__REPLACE_END__"\/>/;
   let indexHTML = fs.readFileSync('./source/index.html').toString();
   const path = request.path ? request.path.split('/') : request.path;
-  const customOpenGraph = ` `;
-  if(path.length > 1 && path[1] === 'post'){
-    customOpenGraph = `
-      <title>Moonshot ${path[2]}</title>
+  const customOpenGraph = `
+      <title>Moonshot 位置在${path[2]}</title>
       <meta
         name="description"
         content="位置在${path[2]}的描述~> Ai art Linebot Group"
@@ -17,18 +15,7 @@ exports.host = functions.https.onRequest((request, response) => {
       <meta property="og:description" content="針對 ${path[2]} 處理的描述 Ai art Linebot Group" />
       <meta property="og:image" content="logo.png" />
     `;
-  }else{
-    customOpenGraph = `
-    <title>Moonshot 位置在${path}</title>
-    <meta
-      name="description"
-      content="位置在${path}的描述~> Ai art Linebot Group"
-    />
-    <meta property="og:title" content="Moonshot 位置在${path}" />
-    <meta property="og:description" content="針對 ${path} 處理的描述 Ai art Linebot Group" />
-    <meta property="og:image" content="logo.png" />
-  `;
-  }
+  
 
 
   indexHTML = indexHTML.replace(META_PLACEHOLDER, customOpenGraph);
