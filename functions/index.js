58f44f6adb95b0a1c689a12c9a113b1269ca5f30
wood.wu <kilokingw@gmail.com>
2023-06-20 17:00:27 +0800

    wip Dynamic OG-Tags

--- functions/index.js
+++ functions/index.js
@@ -5,17 +5,17 @@ exports.host = functions.https.onRequest((request, response) => {
   const META_PLACEHOLDER = /<meta name="__REPLACE_START__"\/>.*<meta name="__REPLACE_END__"\/>/;
   let indexHTML = fs.readFileSync('./source/index.html').toString();
   const path = request.path;
-
+  console.log(request.body)
   const customOpenGraph = `
-<title>Moonshot 位置在${path}</title>
-<meta
-  name="description"
-  content="位置在${path}的描述~> Ai art Linebot Group"
-/>
-<meta property="og:title" content="Moonshot 位置在${path}" />
-<meta property="og:description" content="針對 ${path} 處理的描述 Ai art Linebot Group" />
-<meta property="og:image" content="logo.png" />
-`;
+    <title>Moonshot 位置在${path}</title>
+    <meta
+      name="description"
+      content="位置在${path}的描述~> Ai art Linebot Group"
+    />
+    <meta property="og:title" content="Moonshot 位置在${path}" />
+    <meta property="og:description" content="針對 ${path} 處理的描述 Ai art Linebot Group" />
+    <meta property="og:image" content="logo.png" />
+  `;
 
   indexHTML = indexHTML.replace(META_PLACEHOLDER, customOpenGraph);
   response.status(200).send(indexHTML);
