47617908d2aab1b1df1aac577d58bed7269b25f1
wood.wu <kilokingw@gmail.com>
2023-07-18 20:56:58 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -49,7 +49,13 @@ exports.host = functions.https.onRequest((request, response) => {
 const fetchGalleriesDetail = async (id) => {
 
   return new Promise((resolve, reject) => {
-    const apiUrl = 'https://api-dev.moonshot.today/galleries/';
+    let apiUrl = '';
+    if (process.env.NODE_ENV === 'production') {
+      apiUrl = 'https://api.moonshot.today/galleries/';
+    }else{
+      apiUrl = 'https://api-dev.moonshot.today/galleries/';
+    }
+    
     const url = apiUrl + id;
 
     axios.get(url, { headers: { 'Content-Type': 'application/json' } })
