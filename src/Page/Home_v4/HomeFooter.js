75e27a58ee92b063917797efb2d4d8a11d61aa57
wood.wu <kilokingw@gmail.com>
2023-08-22 21:59:42 +0800

    Update HomeFooter.js

--- src/Page/Home_v4/HomeFooter.js
+++ src/Page/Home_v4/HomeFooter.js
@@ -1,33 +1,84 @@
 import React from 'react'
 import { FaFacebook,FaInstagram,FaYoutube } from "react-icons/fa";
 function HomeFooter() {
+  const about = [
+    {title:'FAQ', link:'/docs/faq'},
+    {title:'Price', link:'/price'},
+    {title:'Getting Started', link:'/docs'},
+  ]
+
+  const soical = [
+    {title:'Instagram', link:'https://www.instagram.com/moonshot_ai/'},
+    {title:'Twitter', link:'https://twitter.com/MoonshotAI_/'},
+    {title:'Facebook', link:'https://www.facebook.com/groups/547117463595869/'},
+    {title:'Youtube', link:'https://www.youtube.com/channel/UCT0noXFjgZ30lLCwZjJYdgQ'},
+  ]
+
+  const join = [
+    {title:'Join line community', link:'https://line.me/ti/g2/0nEqJgPJlUZitp5pYpwLeoPKmswnBuzpRGoBnw?utm_source=invitation&utm_medium=link_copy&utm_campaign=default'}
+  ]
+
   return (
-    <div className='text-white/60 p-4 border-t border-white/20'>
+    <div className='text-white/60 '>
       <div className='flex flex-col text-sm  '>
-        <div className='flex flex-col space-y-2'>
-          <a href="/docs/terms">Terms of Use</a>
-          <a href="/docs/policy">Privacy Policy</a>
-        </div>
-        <div className=' flex  items-center gap-5 mt-4 '>
-              <a href='https://twitter.com/MoonshotAI_' target="_blank" rel="noopener noreferrer" >
-                <img src="https://moonshine.b-cdn.net/msweb/moonshotai/web_icons/twitter-x.svg" className='min-w-3 w-3  object-contain' alt="" />
-              </a>
-              <a href='https://www.youtube.com/channel/UCT0noXFjgZ30lLCwZjJYdgQ' target="_blank" rel="noopener noreferrer" > 
-              <FaYoutube size={16} />
-              </a>
-              <a href='https://www.instagram.com/moonshot_ai' target="_blank" rel="noopener noreferrer" > 
-              <FaInstagram size={16} />
-              </a>
-              <a href='https://www.facebook.com/groups/547117463595869' target="_blank" rel="noopener noreferrer" > 
-              <FaFacebook size={16} />
+
+        <div className=' grid md:grid-cols-4 py-10 space-y-4 md:space-y-0'>
+          <div className='md:flex items-center hidden '>
+            <div className='w-1/3 mx-auto'>
+              <img src="https://moonshine.b-cdn.net/msweb/moonshotai/home_images/logo-2.png" alt="logo" className='max-w-full' />
+            </div>
+          </div>
+          <div className='text-base text-center md:text-left'>
+            <h1 className='text-white font-bold'>About</h1>
+            <div className='flex flex-col space-y-4 mt-4'>
+              {
+                about.map((item,index)=>{
+                  return(
+                    <a href={item.link} className='hover:text-white font-semibold'>{item.title}</a>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className='text-base text-center md:text-left'>
+            <h1 className='text-white font-bold'>Stay tuned!</h1>
+            <div className='flex flex-col space-y-4 mt-4'>
+              {
+                soical.map((item,index)=>{
+                  return(
+                    <a href={item.link} target="_blank" rel="noopener noreferrer" className='hover:text-white font-semibold'>{item.title}</a>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className='text-base text-center md:text-left'>
+            <h1 className='text-white font-bold'>Join line community:</h1>
+            <div className='flex flex-col space-y-4 mt-4'>
+              {
+                join.map((item,index)=>{
+                  return(
+                    <a href={item.link} target="_blank" rel="noopener noreferrer" className='hover:text-white font-semibold'>{item.title}</a>
+                  )
+                })
+              }
+            </div>
+          </div>
+
+            
+          </div>
+
+          <div className='flex flex-col items-center space-y-5 md:space-y-0  md:space-x-7 justify-center text-sm md:text-xs pt-3 md:border-t border-white/30 pb-20 mb:pb-4 '>
+            <div className='flex items-center '>  <div className='hidden md:block'>©</div>
+              <a href='/' className=' w-20 lg:w-20'>
+                <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="logo" className='ml-2 w-full p-0 mt-0 hidden md:block'/>
+                <img src="https://moonshine.b-cdn.net/msweb/moonshotai/home_images/logo-2.png" alt="logo" className='max-w-full block md:hidden' />
               </a>
             </div>
-          <div className=' items-center  text-white gap-2 pt-1 flex  justify-between  mt-14'>
-            <a href='/' className='font-black w-20 lg:w-32'>
-              <img src={process.env.PUBLIC_URL+'/images/ver2_images/mslogo.svg'} alt="" className='w-full'/>
-            </a>
-          <div className='text-xs text-white/60'> Copyright 2023   Moonshot</div>
-        </div>
+            <a href="/docs/terms" className='font-semibold whitespace-nowrap'>Terms of Use</a>
+            <a href="/docs/policy" className='font-semibold whitespace-nowrap'>Privacy Policy</a>
+
+          </div> 
       </div>
   </div>
   )
