8a88fd404c1422b1cea1926bf557b63f347c469c
wood.wu <kilokingw@gmail.com>
2023-12-15 22:34:08 +0800

    wip genpage0.0.5

--- src/Page/Generator/index.js
+++ src/Page/Generator/index.js
@@ -69,9 +69,7 @@ function Index() {
   },[])
 
   const RatioRecElement = ({rv})=>{
-    console.log(rv)
-    const ra = RATIOS[rv - 1];
-    console.log(ra)
+    const ra = rv ?   RATIOS[rv - 1] : RATIOS[4];
     return  <div className={` ${ra?.type === 'h' ?  `w-[36%]`  :  `w-[25%]` } ${ra?.aspect}  h-auto flex justify-center items-center bg-white/50  border-2 border-blue-300/70 text-white/70 text-xl font-bold`}> {ra?.name} </div>
   }
   React.useEffect(() => {
